bd118043d0ceeb5727deb515f4d5a2aaf4f5fac8
Rramirezch <rodrigora05@yahoo.com>
2026-03-07 21:14:38 -0500

    se suben archivo del crud topicos

--- foro_hub/src/main/java/com/alura_foro_hub/foro_hub/direccion/Direccion.java
+++
@@ -1,9 +0,0 @@
-package com.alura_foro_hub.foro_hub.direccion;
-
-public record Direccion(
-        String calle,
-        String barrio,
-        String ciudad,
-        String departamento
-) {
-}
--- foro_hub/src/main/java/com/alura_foro_hub/foro_hub/usuario/Cargo.java
+++
@@ -1,7 +0,0 @@
-package com.alura_foro_hub.foro_hub.usuario;
-
-public enum Cargo {
-        ESTUDIANTE,
-        INSTRUCTOR,
-        DIRECTIVO,
-}
